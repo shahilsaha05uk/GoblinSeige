9d6c7fb531c034ef97fb6e5ad66a4895ae658db4
Shahil Saha <s015695k@student.staffs.ac.uk>
2024-06-06 13:14:33 +0100

    additions

---
+++ Source/TowerDefenceGame/BaseClasses/BaseController.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "BaseController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOWERDEFENCEGAME_API ABaseController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Component")
+	class UAudioComponent* mAudioComp;
+
+	ABaseController();
+
+	virtual void BeginPlay() override;
+
+	virtual void SetupInputComponent() override;
+
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void BindControls(class UEnhancedInputComponent* EnhancedInputComponent);
+	
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void Init();
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void InitSound();
+};
