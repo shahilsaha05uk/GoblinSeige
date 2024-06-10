4eae94ced9235f253f8827789c754ee796770a5e
Shahil Saha <s015695k@student.staffs.ac.uk>
2024-06-10 13:05:21 +0100

    xs

---
+++ Source/TowerDefenceGame/Managers/PhaseManager.cpp
@@ -0,0 +1,85 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PhaseManager.h"
+#include "Kismet/GameplayStatics.h"
+#include "TowerDefenceGame/GameModeClasses/TowerDefenceGameGameModeBase.h"
+#include "TowerDefenceGame/SubsystemClasses/GameSubsystem.h"
+
+void APhaseManager::BeginPlay()
+{
+	Super::BeginPlay();
+	mGameSubsystem = GetGameInstance()->GetSubsystem<UGameSubsystem>();
+
+	LoadPhase();
+}
+
+void APhaseManager::Init_Implementation(ATowerDefenceGameGameModeBase* GameMode)
+{
+	mGameMode = GameMode;
+}
+
+bool APhaseManager::LoadPhase_Implementation()
+{
+	FLatentActionInfo info;
+	info.CallbackTarget = this;
+	info.Linkage = 0;
+
+	// unloading the last phase
+	if(mPhaseDetails.Contains(mCurrentPhase))
+	{
+		auto const PhaseDetails = mPhaseDetails[mCurrentPhase];
+		UGameplayStatics::UnloadStreamLevelBySoftObjectPtr(GetWorld(), PhaseDetails.LevelMap, info, true);
+	}
+
+	// increment the phase count
+	mCurrentPhase++;
+	
+	// loading the next phase
+	if(mPhaseDetails.Contains(mCurrentPhase))
+	{
+		info.ExecutionFunction = FName("OnPhaseLoaded");
+		mCurrentPhaseDetails = mPhaseDetails[mCurrentPhase];
+		UGameplayStatics::LoadStreamLevelBySoftObjectPtr(GetWorld(), mCurrentPhaseDetails.LevelMap, true, true, info);
+		return true;
+	}
+
+	return false;
+}
+void APhaseManager::OnPhaseLoaded_Implementation()
+{
+	UE_LOG(LogTemp, Warning, TEXT("Loading Finish"));
+	UpdateTargets();
+	mGameSubsystem->OnPhaseReadyToPlay.Broadcast(mCurrentPhase, mCurrentPhaseDetails);
+}
+
+void APhaseManager::TargetDestroyed_Implementation()
+{
+	if(mGameSubsystem) mGameSubsystem->OnTargetDestroyed.Broadcast();
+
+	if(mTotalTargetsToDestroy <= 0)
+	{
+		OnNoTargetsLeft.Broadcast();
+
+		if(!LoadPhase())
+		{
+			mGameMode->MakeDecision();
+		}
+		else
+		{
+			mGameSubsystem->OnPhaseComplete.Broadcast(mCurrentPhase);
+		}
+	}
+}
+
+void APhaseManager::UpdatePhase_Implementation()
+{
+}
+
+void APhaseManager::UpdateTargets_Implementation()
+{
+	if(mPhaseDetails.Contains(mCurrentPhase))
+	{
+		mTotalTargetsToDestroy = mPhaseDetails[mCurrentPhase].TargetsToDestroy;
+	}
+}
---
+++ Source/TowerDefenceGame/Managers/PhaseManager.h
@@ -0,0 +1,78 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "TowerDefenceGame/SupportClasses/StructClass.h"
+#include "PhaseManager.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnNoTargetsLeftSignature);
+
+UCLASS()
+class TOWERDEFENCEGAME_API APhaseManager : public AActor
+{
+	GENERATED_BODY()
+
+private:
+
+	UPROPERTY(EditDefaultsOnly)
+	int mTotalTargetsToDestroy = 0;
+	UPROPERTY(EditDefaultsOnly, Category = "Phase")
+	int mCurrentPhase = 0;
+	UPROPERTY(EditDefaultsOnly, Category = "Phase")
+	int mFinalPhase = 2;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Phase")
+	TArray<ULevel*> LevelsToSelect;
+public:
+
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnNoTargetsLeftSignature OnNoTargetsLeft;
+	
+	UPROPERTY(BlueprintReadOnly)
+	class ATowerDefenceGameGameModeBase* mGameMode;
+	UPROPERTY(BlueprintReadOnly)
+	class UGameSubsystem* mGameSubsystem;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Phase", meta = (ToolTip = "Key: Phase; Value: Phase Details"))
+	TMap<int, FPhaseDetails> mPhaseDetails;
+	
+	UPROPERTY(BlueprintReadOnly, Category = "Phase")
+	FPhaseDetails mCurrentPhaseDetails;
+
+	virtual void BeginPlay() override;
+
+	//Privates
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void Init(class ATowerDefenceGameGameModeBase* GameMode);
+
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	bool LoadPhase();
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void TargetDestroyed();
+	UFUNCTION(BlueprintCallable, BlueprintNativeEvent)
+	void UpdatePhase();
+	
+	// on loading/unloading finish
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void OnPhaseLoaded();
+
+	// Updaters
+	UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
+	void UpdateTargets();
+
+	//Getters
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	int GetTotalTargetsToDestroy() {return mTotalTargetsToDestroy;}
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	int GetCurrentPhase() { return mCurrentPhase; }
+	UFUNCTION(BlueprintPure, BlueprintCallable)
+	int GetTotalPhases() {return mFinalPhase; }
+
+	// Checkers
+	UFUNCTION(BlueprintPure)
+	bool HasDestroyedAllTheTargets() const {return (mTotalTargetsToDestroy == 0);}
+	UFUNCTION(BlueprintPure)
+	bool HasPhasesLeft() const { return mCurrentPhase <= mFinalPhase; }
+};
